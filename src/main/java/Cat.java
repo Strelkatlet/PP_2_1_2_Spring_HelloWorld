8b7127b5a8a93897d5c049ea624de2d5de7f4d37
Strelkatlet <ignatievwa@gmail.com>
2022-07-28 17:43:09 +0300

    Решение 2.1.2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessege(String may) {
+        this.message = may;
+    }
+}
